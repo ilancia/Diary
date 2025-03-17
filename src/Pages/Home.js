65c1f638089831d6dc85456a9193b166a0b9e552
ilancia <ilancia1590@naver.com>
2025-03-18 08:21:13 +0900

    서버를 이용한 get기능 사용

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Create } from './Create'
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import { Read } from './Read';
 import Header from '../component/Header';
 import "./Home.css"
+import axios from 'axios';
 
 export default function Home() {
     const navigate = useNavigate();
@@ -13,6 +14,16 @@ export default function Home() {
     const rotary = location.pathname == "/" ? goToCreate : goToHome;
     const [diaryList, setDiaryList] = useState([]);
     const isNotEmpty = location.pathname == "/" && diaryList.length > 0
+    const [test, setTest] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://api.cocktailfunding.com/api/v2/common/notice")
+            .then((response) => {
+                setTest(response.data.body);
+            }).catch((Error) => {
+                console.log(Error);
+            })
+    }, [])
 
     const checkbox = (e, id) => {
         const checkedList = diaryList.map(diary => {
@@ -33,13 +44,16 @@ export default function Home() {
                 <div class="name">유정호</div>
                 <Header rotary={rotary} location={location} />
             </div>
+            <p></p>
+            {location.pathname == "/" ? test.map((test) => <div class='home-title' onClick={() => navigate(`/Read/${test.id}`)} ><div class = 'to-read'>{test.title}</div></div>) :null}
+
             {isNotEmpty ? <button class="remove-button" onClick={() => { handleRemoveDiary() }}>삭제</button> : null}
             {isNotEmpty ? diaryList.map((diary) => (<div class="home-title"> <div class="to-read" onClick={() => navigate(`/Read/${diary.id}`)}>{diary.title}</div>|
                 <input class="check-box" type='checkbox' checked={diary.ischeck} onChange={(e) => checkbox(e, diary.id)} />
             </div>)) : null}
             <Routes>
                 <Route path='/Create' element={<Create setDiaryList={setDiaryList} />} />
-                <Route path='/Read/:id' element={<Read setDiaryList={setDiaryList} navigate={navigate} diaryList={diaryList} />} />
+                <Route path='/Read/:id' element={<Read setDiaryList={setDiaryList} navigate={navigate} diaryList={diaryList} test={test} />} />
             </Routes>
         </div>
     )
--- src/Pages/Read.js
+++ src/Pages/Read.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
+import parse from 'html-react-parser'
 import "./Read.css"
 
 export const Read = (props) => {
@@ -7,6 +8,7 @@ export const Read = (props) => {
   const [foundDiary, setFoundDiary] = useState(props.diaryList.find(d => (d.id) == id));
   const copiedDiary = [...props.diaryList];
   const listFindIndex = props.diaryList.findIndex((dia) => dia.id == foundDiary.id);
+  const foundTest = props.test.find(d => d.id == id);
 
   const updateList = () => {
     if (!foundDiary.title.trim() || !foundDiary.content.trim()) {
@@ -24,11 +26,18 @@ export const Read = (props) => {
     setFoundDiary({ ...foundDiary, [name]: value, });
   }
 
+  const test = foundTest.body;
+  console.log(foundTest.body)
+
   return (
     <div>
       <button class="update" onClick={() => { updateList() }} >수정</button>
-      <input class="title" name="title" spellCheck="false" value={foundDiary.title} onChange={onChange} placeholder='제목' />
-      <textarea class="content" name="content" spellCheck="false" value={foundDiary.content} onChange={onChange} placeholder='내용' />
+      <input class="title" spellCheck="false" value={foundTest.title}/>
+      {/* <input type="text" class="content" spellCheck="false" value={} /> */}
+      <div class= 'uproaded-content'>{parse(test)}</div>
+      {/* <textarea class="content" spellCheck="false" value={test} /> */}
+      {/* <input class="title" name="title" spellCheck="false" value={foundDiary.title} onChange={onChange} placeholder='제목' />
+      <textarea class="content" name="content" spellCheck="false" value={foundDiary.content} onChange={onChange} placeholder='내용' /> */}
     </div>
   )
 }
