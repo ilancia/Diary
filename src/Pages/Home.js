9d0a2219c6e9d0e417cf1c5599971b42142b484e
ilancia <ilancia1590@naver.com>
2025-03-18 19:14:10 +0900

    api설정

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,13 +11,13 @@ export default function Home() {
     const goToHome = () => { navigate('/') };
     const goToCreate = () => { navigate('/Create') };
     const location = useLocation();
-    const rotary = location.pathname == "/" ? goToCreate : goToHome;
+    const rotary = location.pathname === "/" ? goToCreate : goToHome;
     const [diaryList, setDiaryList] = useState([]);
-    const isNotEmpty = location.pathname == "/" && diaryList.length > 0
+    const isNotEmpty = location.pathname === "/" && diaryList.length > 0
     const [test, setTest] = useState([]);
 
     useEffect(() => {
-        axios.get("https://api.cocktailfunding.com/api/v2/common/notice")
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/common/notice`)
             .then((response) => {
                 setTest(response.data.body);
             }).catch((Error) => {
@@ -27,14 +27,14 @@ export default function Home() {
 
     const checkbox = (e, id) => {
         const checkedList = diaryList.map(diary => {
-            return diary.id == id ? { id: id, title: diary.title, content: diary.content, ischeck: e.target.checked } : diary
+            return diary.id === id ? { id: id, title: diary.title, content: diary.content, ischeck: e.target.checked } : diary
         });
         setDiaryList(checkedList);
     }
 
     const handleRemoveDiary = () => {
         const remainList = diaryList.filter((diary) => diary.ischeck !== true)
-        if (remainList.length == diaryList.length) alert("삭제하고싶은 것을 선택하세요");
+        if (remainList.length === diaryList.length) alert("삭제하고싶은 것을 선택하세요");
         else setDiaryList(remainList);
     };
 
@@ -45,7 +45,7 @@ export default function Home() {
                 <Header rotary={rotary} location={location} />
             </div>
             <p></p>
-            {location.pathname == "/" ? test.map((test) => <div class='home-title' onClick={() => navigate(`/Read/${test.id}`)} ><div class = 'to-read'>{test.title}</div></div>) :null}
+            {location.pathname === "/" ? test.map((test) => <div class='home-title' onClick={() => navigate(`/Read/${test.id}`)} ><div class = 'to-read'>{test.title}</div></div>) :null}
 
             {isNotEmpty ? <button class="remove-button" onClick={() => { handleRemoveDiary() }}>삭제</button> : null}
             {isNotEmpty ? diaryList.map((diary) => (<div class="home-title"> <div class="to-read" onClick={() => navigate(`/Read/${diary.id}`)}>{diary.title}</div>|
