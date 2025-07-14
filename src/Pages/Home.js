4c1007216ca727805dcd86f2342d184df6a88483
ilancia <ilancia1590@naver.com>
2025-07-14 16:05:27 +0900

    diary 수정

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Create } from './Create'
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import { Read } from './Read';
 import Header from '../component/Header';
 import "./Home.css"
-import axios from 'axios';
 
 export default function Home() {
     const navigate = useNavigate();
@@ -14,16 +13,6 @@ export default function Home() {
     const rotary = location.pathname === "/" ? goToCreate : goToHome;
     const [diaryList, setDiaryList] = useState([]);
     const isNotEmpty = location.pathname === "/" && diaryList.length > 0
-    const [test, setTest] = useState([]);
-
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_SERVER_URL}/common/notice`)
-            .then((response) => {
-                setTest(response.data.body);
-            }).catch((Error) => {
-                console.log(Error);
-            })
-    }, [])
 
     const checkbox = (e, id) => {
         const checkedList = diaryList.map(diary => {
@@ -40,20 +29,17 @@ export default function Home() {
 
     return (
         <div>
-            <div class="header-wrapper">
-                <div class="name">유정호</div>
+            <div className="header-wrapper">
+                <div className="name">유정호</div>
                 <Header rotary={rotary} location={location} />
             </div>
-            <p></p>
-            {location.pathname === "/" ? test.map((test) => <div class='home-title' onClick={() => navigate(`/Read/${test.id}`)} ><div class = 'to-read'>{test.title}</div></div>) :null}
-
-            {isNotEmpty ? <button class="remove-button" onClick={() => { handleRemoveDiary() }}>삭제</button> : null}
-            {isNotEmpty ? diaryList.map((diary) => (<div class="home-title"> <div class="to-read" onClick={() => navigate(`/Read/${diary.id}`)}>{diary.title}</div>|
-                <input class="check-box" type='checkbox' checked={diary.ischeck} onChange={(e) => checkbox(e, diary.id)} />
+            {isNotEmpty ? <button className="remove-button" onClick={() => { handleRemoveDiary() }}>삭제</button> : null}
+            {isNotEmpty ? diaryList.map((diary) => (<div className="home-title"> <div className="to-read" onClick={() => navigate(`/Read/${diary.id}`)}>{diary.title}</div>|
+                <input className="check-box" type='checkbox' checked={diary.ischeck} onChange={(e) => checkbox(e, diary.id)} />
             </div>)) : null}
             <Routes>
                 <Route path='/Create' element={<Create setDiaryList={setDiaryList} />} />
-                <Route path='/Read/:id' element={<Read setDiaryList={setDiaryList} navigate={navigate} diaryList={diaryList} test={test} />} />
+                <Route path='/Read/:id' element={<Read setDiaryList={setDiaryList} navigate={navigate} diaryList={diaryList} />} />
             </Routes>
         </div>
     )
