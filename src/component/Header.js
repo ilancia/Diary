a2d24d32663a1518a693dda42b5a831a9129cad2
ilancia <ilancia1590@naver.com>
2025-07-14 16:29:25 +0900

    diary 수정.2

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,9 @@
-import React from 'react'
-
 export default function Header(props) {
   const Home = props.location.pathname == "/" ? "글쓰기" : "홈으로";
 
   return (
     <div>
-      <button class="write" onClick={props.rotary}>{Home}</button>
+      <button className="write" onClick={props.rotary}>{Home}</button>
     </div>
   )
 }
